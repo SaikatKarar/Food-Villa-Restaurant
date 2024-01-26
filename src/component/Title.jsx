027a249e96fe1aff5c9b70cce51b11ad94655f2e
SaikatKarar <kararsaikat@gmail.com>
2024-01-26 20:03:29 +0530

    first commit

---
+++ src/component/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div>
+            <h1 id='title' key="h2">Food Villa</h1>
+        </div>
+    )
+}
+
+export default Title
